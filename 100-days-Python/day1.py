1b4c3bfab9653ad7a409528a70cc1a3af9265429
Rupeshkedia <raghavkedia505@gmail.com>
2026-05-12 22:56:52 +0530

    day1

---
+++ 100-days-Python/day1.py
@@ -0,0 +1,6 @@
+#Check whether a given number is multiple of five or not.
+num = int(input("enter a number:"))
+if num % 5 == 0:
+  print("it is multiple of 5")
+else:
+  print("it is not a multiple of 5")
