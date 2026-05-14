e6d429727245608d19ec2ec412b0ea2b27653e50
Rupeshkedia <raghavkedia505@gmail.com>
2026-05-15 01:45:52 +0530

    day4

--- 100-days-Python/day4.py
+++ 100-days-Python/day4.py
@@ -1,5 +1,5 @@
 #Find whether a given year is a leap year or not.
-year = int(input("enter a year"))
+year = int(input("enter a year :"))
 if(year % 4 == 0 and year % 100 != 0) or (year %400 == 0):
   print("it is a leap year")
 else:
