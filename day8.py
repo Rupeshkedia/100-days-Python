03a2b8cd90b959b2e0eac177e8fa790e52b63b47
Rupeshkedia <raghavkedia505@gmail.com>
2026-05-19 00:42:37 +0530

    day8

---
+++ day8.py
@@ -0,0 +1,11 @@
+num = int(input("enter a number:"))
+flag = 0
+
+for i in range(2, num):
+  if num % i == 0:
+    flag = 1
+    break
+if flag == 0 and num > 1:
+  print("prime number")
+else:
+  print("not a prime number")
