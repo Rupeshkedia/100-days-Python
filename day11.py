60deaf00d1d270759b3a9454b3b0ca4f8348d80f
Rupeshkedia <raghavkedia505@gmail.com>
2026-05-23 23:39:40 +0530

    day11

---
+++ day11.py
@@ -0,0 +1,15 @@
+#Ques 10
+n = 5
+
+for i in range(n):
+
+    for j in range(1, n - i + 1):
+        print(j, end="")
+
+    for j in range(2 * i):
+        print("*", end="")
+
+    for j in range(n - i, 0, -1):
+        print(j, end="")
+
+    print()
